8eb2eb946a5ef807a46fdfb5ac30240330ffc611
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 12:54:21 +0000

    Nov 28, 2024, 6:24 PM

--- RN-food-app/App.js
+++ RN-food-app/App.js
@@ -18,7 +18,6 @@ export default function App() {
             component={CategoriesScreen} 
           />
         </Stack.Navigator>
-        {/* <CategoriesScreen /> */}
       </NavigationContainer>
     </>
   );
