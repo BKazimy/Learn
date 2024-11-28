c30dd7a9ca3cb2e9920b6e47a90e32bb1a61c2d9
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 10:24:44 +0000

    Nov 28, 2024, 3:54 PM

--- RN-food-app/screens/CategoriesScreen.js
+++ RN-food-app/screens/CategoriesScreen.js
@@ -10,7 +10,7 @@ function CategoriesScreen() {
     return <FlatList 
         data={CATEGORIES} 
         keyExtractor={(item) => item.id} 
-        re
+        renderItem={renderCategoryItem}
     />;
 }
 
