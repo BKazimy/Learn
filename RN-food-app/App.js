1f28199388b3145990dc916d674506ca76284de3
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 12:14:20 +0000

    Nov 28, 2024, 5:44 PM

---
+++ RN-food-app/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar></StatusBar>
+      <CategoriesScreen />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
