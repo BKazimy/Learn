4428599db7323dd3f67f9cd1c9d91b76273a3129
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 10:37:22 +0000

    Nov 28, 2024, 4:07 PM

--- RN-food-app/screens/CategoriesScreen.js
+++ RN-food-app/screens/CategoriesScreen.js
@@ -6,7 +6,7 @@ import { FlatList } from 'react-native';
 function renderCategoryItem(itemData) {
     return <CategoryGridTile 
         title={itemData.item.title} 
-        itme
+        color={itemData.item.color}
     />;
 }
 
