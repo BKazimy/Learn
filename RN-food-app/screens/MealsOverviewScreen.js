2113a3dc23d011d1d39dff893b9c0b54752c3b6e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 19:12:32 +0000

    Nov 29, 2024, 12:42 AM

--- RN-food-app/screens/MealsOverviewScreen.js
+++ RN-food-app/screens/MealsOverviewScreen.js
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 
 function MealsOverviewScreen(){ //{ route }) {
@@ -17,7 +18,7 @@ function MealsOverviewScreen(){ //{ route }) {
             <FlatList 
                 data={displayedMeals} 
                 keyExtractor={(item) => item.id}
-                renderItem={renderMealItem}
+                renderItem={MealItem}
             />
         </View>
     );
