931462ce454d6eec8767c33aca63b52e2dbd827e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 10:22:19 +0000

    Nov 28, 2024, 3:52 PM

--- RN-food-app/screens/CategoriesScreen.js
+++ RN-food-app/screens/CategoriesScreen.js
@@ -3,7 +3,7 @@ import { CATEGORIES } from '../data/dummy-data'
 import { FlatList } from 'react-native';
 
 function CategoriesScreen() {
-    return <FlatList data />;
+    return <FlatList data={CATEGORIES} />;
 }
 
 export default CategoriesScreen;
