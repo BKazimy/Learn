c82aa0257d21d33e690ac53e834c487846864fd4
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 18:57:18 +0000

    Nov 29, 2024, 12:27 AM

--- RN-food-app/screens/MealsOverviewScreen.js
+++ RN-food-app/screens/MealsOverviewScreen.js
@@ -2,7 +2,7 @@ import { View, Text, StyleSheet } from "react-native";
 import { MEALS } from "../data/dummy-data";
 
 
-function MealsOverviewScreen({ navigation }) {
+function MealsOverviewScreen({ route }) {
     return <View style={styles.container}>
         <Text>Meals Overview Screen!</Text>
     </View>
