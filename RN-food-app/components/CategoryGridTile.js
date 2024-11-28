6e94d8b19c2c83b214ca71d06964f873e9a557fa
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 12:01:17 +0000

    Nov 28, 2024, 5:31 PM

--- RN-food-app/components/CategoryGridTile.js
+++ RN-food-app/components/CategoryGridTile.js
@@ -1,4 +1,4 @@
-import { Pressable, View, Text, StyleSheet } from "react-native";
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
 function CategoryGridTile({ title, color }) {
     return (
