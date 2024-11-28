e2d93f56c0f278695cc924d16b451762b981c356
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 11:13:23 +0000

    Nov 28, 2024, 4:43 PM

--- RN-food-app/components/CategoryGridTile.js
+++ RN-food-app/components/CategoryGridTile.js
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowOffset: { width: 0, height: 2},
         shadowRadius: 8,
-        overflow: 'h'
+        overflow: 'hidden',
     },
 
     button: {
