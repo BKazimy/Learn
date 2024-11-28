531fc8022df8bab3ea135b0513fd748767b7faa5
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 19:25:57 +0000

    Nov 29, 2024, 12:55 AM

---
+++ RN-food-app/screens/MealsOverviewScreen.js
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from "react-native";
+import { useRoute } from "@react-navigation/native";
+
+import { MEALS } from "../data/dummy-data";
+
+
+function MealsOverviewScreen(){ //{ route }) {
+    // const route = ;
+    const catId = useRoute().route.params.categoryId;
+
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    })
+
+    return <View style={styles.container}>
+        <Text>Meals Overview Screen! #{catId}</Text>
+    </View>
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    },
+})
