6dd18e3eb3f83e07c15f0f397dc7053e4779ce45
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 12:14:45 +0000

    Nov 28, 2024, 5:44 PM

--- RN-food-app/App.js
+++ RN-food-app/App.js
@@ -5,7 +5,7 @@ import CategoriesScreen from './screens/CategoriesScreen';
 export default function App() {
   return (
     <>
-      <StatusBar style='' />
+      <StatusBar style='light' />
       <CategoriesScreen />
     </>
   );
