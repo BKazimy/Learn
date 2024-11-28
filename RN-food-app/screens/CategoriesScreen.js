59146d4b80afe4241f5762f8c4d38f7e41c3b886
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-28 18:55:36 +0000

    Nov 29, 2024, 12:25 AM

--- RN-food-app/screens/CategoriesScreen.js
+++ RN-food-app/screens/CategoriesScreen.js
@@ -7,7 +7,9 @@ function CategoriesScreen({ navigation }) {
 
     function renderCategoryItem(itemData) {
         function pressHandler() {
-            navigation.navigate('Meal Overview', {});
+            navigation.navigate('Meal Overview', {
+                cate
+            });
         }
     
         return <CategoryGridTile 
