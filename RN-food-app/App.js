d10d268b1d690f936ab56e56063607da45f21832
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-29 18:29:27 +0000

    Nov 29, 2024, 11:59 PM

--- RN-food-app/App.js
+++ RN-food-app/App.js
@@ -21,6 +21,7 @@ export default function App() {
               title: 'All Categories',
               headerStyle: { backgroundColor: '#351401' },
               headerTintColor: 'white',
+              contentStyle: {  }
             }}
           />
           <Stack.Screen 
