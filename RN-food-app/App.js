1f8c645b40701190636945b2770a117cc97ed334
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-29 18:32:04 +0000

    Nov 30, 2024, 12:02 AM

--- RN-food-app/App.js
+++ RN-food-app/App.js
@@ -28,7 +28,7 @@ export default function App() {
           <Stack.Screen 
             name='Meal Overview' 
             component={MealsOverviewScreen}
-            options={[globalOptions, {backgroundColor: 'grey'
+            options={[globalOptions, {backgroundColor: ''
             }]}
           />
         </Stack.Navigator>
