72c7607430dc5667c077340d82b8a3ef6467def1
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-29 17:12:51 +0000

    Nov 29, 2024, 10:42 PM

--- RN-food-app/components/MealItem.js
+++ RN-food-app/components/MealItem.js
@@ -3,7 +3,9 @@ import { View, Text, Pressable } from "react-native";
 function MealItem({ title }) {
     return (
         <View>
+            <Pressable>
             <Text>{title}</Text>
+            </Pressable>
         </View>
     );
 }
