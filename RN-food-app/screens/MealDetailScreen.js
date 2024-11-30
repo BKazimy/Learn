9adb4490f13b048e9271a87a6ec0e45f9731f3c3
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 07:43:27 +0000

    Nov 30, 2024, 1:13 PM

--- RN-food-app/screens/MealDetailScreen.js
+++ RN-food-app/screens/MealDetailScreen.js
@@ -11,7 +11,7 @@ function MealDetailScreen({ route }) {
     const selectedMeal = MEALS.find((meal) => meal.id == mealId);
 
     return (
-        <ScrollView>
+        <ScrollView style={styles.rootContainer}>
             <Image 
                 style={styles.image}
                 source={{uri: selectedMeal.imageUrl}} 
