46820b48b6ba58f3469d8650d3b4647bd4d6d8df
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 06:55:51 +0000

    Nov 30, 2024, 12:25 PM

---
+++ RN-food-app/components/mealDetail/Subtitle.js
@@ -0,0 +1,11 @@
+
+
+function Subtitle() {
+    return (
+    <View style={styles.subtitleContainer}>
+        <Text style={styles.subtitle}>Steps:</Text>
+    </View>
+    );
+}
+
+export default Subtitle;
