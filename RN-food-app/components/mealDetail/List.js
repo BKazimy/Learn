b4b6420144cbb292c79ed3bcea332b4b4bd82894
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 07:31:00 +0000

    Nov 30, 2024, 1:01 PM

---
+++ RN-food-app/components/mealDetail/List.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function List({ data }) {
+    return data.map((data))
+}
+
+export default List;
+
+const styles = StyleSheet.create({
+
+})
