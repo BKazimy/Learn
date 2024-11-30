5cb2581b8249f44537630bd174422375b5fcb130
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 07:31:42 +0000

    Nov 30, 2024, 1:01 PM

--- RN-food-app/components/mealDetail/List.js
+++ RN-food-app/components/mealDetail/List.js
@@ -1,7 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
 
 function List({ data }) {
-    return data.map((data))
+    return data.map((dataPoint) => (
+        <View>
+            <Text key={dataPoint} >{dataPoint}</Text>
+        </View>
+    ))
 }
 
 export default List;
