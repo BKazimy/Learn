03e228e756e593a46c79cc81faa36fe957aeba5c
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 09:29:59 +0000

    Nov 30, 2024, 2:59 PM

--- RN-food-app/App.js
+++ RN-food-app/App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import { Button } from 'react-native-web';
 
 const Stack = createNativeStackNavigator();
 
@@ -42,7 +43,7 @@ export default function App() {
             options={{
               title: "Meal Detail",
               headerRight: () => {
-                return <Text>In the header.</Text>
+                return <Button title='' />
               }
             }}
           />
