4eb19c5a29609a4f44d1c57c3b3bfe62bb2a987d
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 06:19:51 +0000

    Nov 30, 2024, 11:49 AM

--- RN-food-app/screens/MealDetailScreen.js
+++ RN-food-app/screens/MealDetailScreen.js
@@ -18,7 +18,7 @@ function MealDetailScreen({ route }) {
                 affordability={selectedMeal.affordability}
             />
             <Text>ingredients:</Text>
-            
+            {selectedMeal.ingredients.map(ingredients => <Text></Text>)}
         </View>
     );
 }
