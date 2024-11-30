d58967d88b925c5cacad14b7bbf7efb8cb76fd6a
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 06:38:18 +0000

    Nov 30, 2024, 12:08 PM

--- RN-food-app/components/mealDetails.js
+++ RN-food-app/components/mealDetails.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native";
 function MealDetails({ duration, complexity, affordability, style, textStyle }) {
     return (
         <View style={[styles.details, style]}>
-            <Text style={styles.detailItem}>{duration}m</Text>
+            <Text style={[styles.detailItem, textStyle]}>{duration}m</Text>
             <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
             <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
         </View>
