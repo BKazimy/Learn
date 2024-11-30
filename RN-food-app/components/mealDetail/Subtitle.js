30e68ca8d6148d19422145afdd73b355270196b6
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 07:25:58 +0000

    Nov 30, 2024, 12:55 PM

--- RN-food-app/components/mealDetail/Subtitle.js
+++ RN-food-app/components/mealDetail/Subtitle.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from "react-native";
 
-function Subtitle() {
+function Subtitle({ chil}) {
     return (
     <View style={styles.subtitleContainer}>
         <Text style={styles.subtitle}>Steps:</Text>
