75cf24b07ec925fd4c68c98d220aaf84eba13b16
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 15:57:14 +0000

    Nov 30, 2024, 9:27 PM

--- RN-food-app/screens/MealDetailScreen.js
+++ RN-food-app/screens/MealDetailScreen.js
@@ -28,7 +28,7 @@ function MealDetailScreen({ route, navigation }) {
             headerRight: () => {
                 return <Button 
                     title="Tap me" 
-                    onPress={() => console.log("logged!")}
+                    onPress={() => console.warn("logged!")}
                 />
             }
         });
