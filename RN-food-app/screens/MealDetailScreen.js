e6f93540b94578eb9866b87dd5f4a74dd11e94b5
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 16:25:53 +0000

    Nov 30, 2024, 9:55 PM

--- RN-food-app/screens/MealDetailScreen.js
+++ RN-food-app/screens/MealDetailScreen.js
@@ -26,7 +26,7 @@ function MealDetailScreen({ route, navigation }) {
     useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => {
-                console.log("header touchable!")
+                console.log("header touchable rendered!")
                 return <TouchableOpacity onPress={() => console.log("header touchable!")}>
                     <Text style={{ padding: 10, color: 'blue' }}>Header touch</Text>
                 </TouchableOpacity>
