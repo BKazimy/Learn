8fdd11b136ad70bb94857c470cce30887e95da76
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 06:38:34 +0000

    Nov 30, 2024, 12:08 PM

--- RN-food-app/components/mealDetails.js
+++ RN-food-app/components/mealDetails.js
@@ -4,7 +4,7 @@ function MealDetails({ duration, complexity, affordability, style, textStyle }) 
     return (
         <View style={[styles.details, style]}>
             <Text style={[styles.detailItem, textStyle]}>{duration}m</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={[styles.detailItem, textStyle]}>{complexity.toUpperCase()}</Text>
             <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
         </View>
     );
