a3766d2db059b14b163157a2e13da8e30fa9a495
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 06:08:40 +0000

    Nov 30, 2024, 11:38 AM

--- RN-food-app/components/MealItem.js
+++ RN-food-app/components/MealItem.js
@@ -21,11 +21,11 @@ function MealItem({ id, title, imageURL, duration, complexity, affordability }) 
                         <Image source={{uri: imageURL}} style={styles.image} />
                         <Text style={styles.title}>{title}</Text>
                     </View>
-                    <View style={styles.details}>
-                        <Text style={styles.detailItem}>{duration}m</Text>
-                        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-                        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-                    </View>
+                    <MealDetails 
+                duration={duration}
+                complexity={complexity}
+                affordability={affordability}
+            />
                 </View>
             </Pressable>
         </View>
