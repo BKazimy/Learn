9db9aab1c9d1f548eee737e57a3d07dbe0f50dbf
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 17:37:08 +0000

    Nov 30, 2024, 11:07 PM

--- RN-food-app/components/IconButton.js
+++ RN-food-app/components/IconButton.js
@@ -2,7 +2,7 @@ import { Ionicons } from "@expo/vector-icons";
 import { Pressable } from "react-native";
 
 function IconButton({ onPress}) {
-    return <Pressable>
+    return <Pressable on>
         <Ionicons name="star" size={24} color="white" />
     </Pressable>
 }
