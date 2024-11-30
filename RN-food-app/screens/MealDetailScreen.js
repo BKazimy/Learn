17be013d68c4064ff5f0af6ea77ee34a4e321503
Besmella Kazimy <b.kazimy5@gmail.com>
2024-11-30 05:40:32 +0000

    Nov 30, 2024, 11:10 AM

--- RN-food-app/screens/MealDetailScreen.js
+++ RN-food-app/screens/MealDetailScreen.js
@@ -1,7 +1,7 @@
 import { Text } from "react-native";
 
 function MealDetailScreen() {
-    return <Text>This is the Meal Detail screen (Meal id)!</Text>
+    return <Text>This is the Meal Detail screen {id}!</Text>
 }
 
 export default MealDetailScreen;
