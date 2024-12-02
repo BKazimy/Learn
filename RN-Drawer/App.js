855aa2e404e166919bf4cc146d615e5e69e9f03d
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-02 20:18:09 +0000

    Dec 3, 2024, 1:48 AM

--- RN-Drawer/App.js
+++ RN-Drawer/App.js
@@ -24,7 +24,7 @@ export default function App() {
         name='Welcome'
         component={WelcomeScreen}
         options={{
-          tabBar
+          tabBarIcon: ({color, size})
         }}
       />
       <BottomTab.Screen 
