e945620cc28f4541c7758e2270fe4e3750870261
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-02 16:33:56 +0000

    Dec 2, 2024, 10:03 PM

--- RN-Drawer/App.js
+++ RN-Drawer/App.js
@@ -7,7 +7,7 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return <NavigationContainer>
     <Drawer.Navigator>
-      <Drawer.Screen name='Welcome' />
+      <Drawer.Screen name='Welcome' component={Welcome} />
     </Drawer.Navigator>
   </NavigationContainer>;
 }
