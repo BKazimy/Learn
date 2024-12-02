3ee966380a43d520d737baef3e07e5444e2812cb
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-02 20:14:07 +0000

    Dec 3, 2024, 1:44 AM

--- RN-Drawer/App.js
+++ RN-Drawer/App.js
@@ -21,6 +21,7 @@ export default function App() {
       />
       <BottomTab.Screen 
         name='User'
+        component={UserScreen}
       />
     </BottomTab.Navigator>
   </NavigationContainer>
