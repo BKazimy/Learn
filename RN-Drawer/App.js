2bc6874db671d8015bce0fc544dbe71792ad094e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-02 16:50:25 +0000

    Dec 2, 2024, 10:20 PM

--- RN-Drawer/App.js
+++ RN-Drawer/App.js
@@ -18,7 +18,7 @@ export default function App() {
           headerTintColor: 'white',
           drawerLabel: "Welcome screen",
           drawerActiveBackgroundColor: "#f0e1ff",
-          drawerActiveTintColor: "#3c"
+          drawerActiveTintColor: "#3c0a6b",
         }}
       />
       <Drawer.Screen 
