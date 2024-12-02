4a5141973120583f3b4f53fd30b7b92bba83d28d
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-02 21:36:10 +0000

    Dec 3, 2024, 3:06 AM

--- RN-food-app/App.js
+++ RN-food-app/App.js
@@ -14,6 +14,10 @@ const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return <Drawer.Navigator>
+    <Drawer.Screen 
+      name='Categories'
+      component={CategoriesScreen}
+    />
     <Drawer.Screen />
   </Drawer.Navigator>
 }
