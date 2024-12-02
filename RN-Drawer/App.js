0a2de0aaae08a46aa2a7f3be495cc49fdcde6817
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-02 16:47:00 +0000

    Dec 2, 2024, 10:17 PM

--- RN-Drawer/App.js
+++ RN-Drawer/App.js
@@ -9,13 +9,13 @@ const Drawer = createDrawerNavigator();
 
 export default function App() {
   return <NavigationContainer>
-    <Drawer.Navigator >
+    <Drawer.Navigator 
+      
+    >
       <Drawer.Screen 
         name='Welcome' 
         component={WelcomeScreen} 
         options={{
-          headerStyle: { backgroundColor: "#3c0a6b"},
-          headerTintColor: 'white',
           drawerLabel: "Welcome screen"
         }}
       />
