48c9bfc3922ab62c025bf2dd0285e58b8116d987
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-02 21:24:29 +0000

    Dec 3, 2024, 2:54 AM

---
+++ RN-food-app/App.js
@@ -0,0 +1,70 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import MealDetailScreen from './screens/MealDetailScreen';
+import { Button } from 'react-native-web';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return <Drawer.Navigator>
+    <Drawer.Screen />
+  </Drawer.Navigator>
+}
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style='light' />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerStyle: { backgroundColor: '#351401' },
+          headerTintColor: 'white',
+          contentStyle: { backgroundColor: '#3f2f25' },
+        }} >
+          <Stack.Screen 
+            name='MealCategories' 
+            component={DrawerNavigator}
+            options={{
+              title: "All Categories"
+            }}
+          />
+          <Stack.Screen 
+            name='MealOverview' 
+            component={MealsOverviewScreen}
+            // options={({ route, navigation }) => {
+            //   const catId = route.params.categoryId
+            //   return {
+            //     title: catId,
+            //   };
+            // }}
+          />
+          <Stack.Screen 
+            name='MealDetail' 
+            component={MealDetailScreen}
+            options={{
+              title: "Meal Detail",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
