295dca6bd29cd2cd10d6d1f4e9f7fd716412216c
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-02 16:32:12 +0000

    Dec 2, 2024, 10:02 PM

--- RN-Drawer/App.js
+++ RN-Drawer/App.js
@@ -1,8 +1,9 @@
 import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-'
 
 export default function App() {
   return <NavigationContainer>
-    
+
   </NavigationContainer>;
 }
