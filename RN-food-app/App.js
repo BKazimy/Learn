10dcd0fd11c9221f40b72e44ce9bd974d02c2746
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-03 17:50:33 +0000

    Dec 3, 2024, 11:20 PM

--- RN-food-app/App.js
+++ RN-food-app/App.js
@@ -21,7 +21,7 @@ function DrawerNavigator() {
       headerStyle:{ backgroundColor: '#351401'},
       headerTintColor: 'white',
       sceneContainerStyle: { backgroundColor: '#3f2f25'},
-      drawerContentStyle: { backgroundColor: 'white'},
+      drawerContentStyle: { backgroundColor: '#351401'},
       drawerInactiveTintColor: 'white',
       drawerActiveTintColor: '#351401',
       drawerActiveBackgroundColor: '#e4baa1',
