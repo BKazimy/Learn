cd0538a0727461b20092777c45c2af7272145917
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-03 20:08:29 +0000

    Dec 4, 2024, 1:38 AM

--- RN-food-app/components/IconButton.js
+++ RN-food-app/components/IconButton.js
@@ -1,19 +1,17 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Button, Pressable } from "react-native";
+import { Pressable } from "react-native";
 import { StyleSheet } from "react-native";
 
 function IconButton({ onPress, icon, color }) {
-    return <Button 
+    return <Pressable 
         onPress={onPress} 
-        style={{ padding: 20, backgroundColor: 'red' }}
-        // style={({pressed}) => [
-        //     pressed && styles.pressed,
-        //     { borderWidth: 2, borderColor: 'blue', padding: 10 },
-        // ]}
-        title="Add"
-    />
-        {/* <Ionicons name={icon} size={24} color={color} />
-    </Button> */}
+        style={({pressed}) => [
+            pressed && styles.pressed,
+            { borderWidth: 2, borderColor: 'blue', padding: 10 },
+        ]}
+    >
+        <Ionicons name={icon} size={24} color={color} />
+    </Pressable>
 }
 
 export default IconButton;
