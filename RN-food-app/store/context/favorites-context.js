11064ba22fae55272e355c0341a2c5e5cd9cf96e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-03 17:27:43 +0000

    Dec 3, 2024, 10:57 PM

--- RN-food-app/store/context/favorites-context.js
+++ RN-food-app/store/context/favorites-context.js
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 
 export const FavoritesContext = createContext({
-    ids: [1,2,3],
+    ids: ['m1', ],
     addFavorite: (id) => {},
     removeFavorite: (id) => {},
 });
