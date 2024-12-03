4be88b86413fb8a0d46293dfe549b31a23a10554
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-03 17:41:38 +0000

    Dec 3, 2024, 11:11 PM

--- RN-food-app/screens/FavoritesScreen.js
+++ RN-food-app/screens/FavoritesScreen.js
@@ -1,8 +1,13 @@
 import { Text } from "react-native";
 import MealsList from "../components/MealsList/MealsList";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-    return <MealsList items />
+    const favoriteMealCtx = useContext(FavoritesContext);
+    const favoriteMeals = MEALS.filter(meal => favoriteMealCtx.ids.includes(meal.id));
+    return <MealsList items={favoriteMeals} />
 }
 
 export default FavoritesScreen;
