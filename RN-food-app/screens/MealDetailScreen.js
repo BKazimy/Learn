69ef9a1d9068cd91a92ed31780894a0178c0d9fa
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-03 17:16:52 +0000

    Dec 3, 2024, 10:46 PM

--- RN-food-app/screens/MealDetailScreen.js
+++ RN-food-app/screens/MealDetailScreen.js
@@ -14,7 +14,7 @@ import { useContext, useLayoutEffect } from "react";
 import IconButton from "../components/IconButton";
 
 function MealDetailScreen({ route, navigation }) {
-    const favoriteMealCtx = useContext(Favo);
+    const favoriteMealCtx = useContext(FavoritesContext);
 
     const mealId = route.params.mealId;
 
