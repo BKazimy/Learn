487df53ee9ec04e43342f73f1222053fd6e8fdb1
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-03 14:06:09 +0000

    Dec 3, 2024, 7:36 PM

--- RN-food-app/App.js
+++ RN-food-app/App.js
@@ -10,7 +10,7 @@ import MealDetailScreen from './screens/MealDetailScreen';
 import { Button } from 'react-native-web';
 import FavoritesScreen from './screens/FavoritesScreen';
 import { Ionicons } from '@expo/vector-icons';
-import { FavoritesContextProvider } from './'
+import { FavoritesContextProvider } from './store/'
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
