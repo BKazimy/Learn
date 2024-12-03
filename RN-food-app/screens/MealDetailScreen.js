62e7dff4c279d7ffd7ad646ae5261a05e1df7108
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-03 17:57:17 +0000

    Dec 3, 2024, 11:27 PM

--- RN-food-app/screens/MealDetailScreen.js
+++ RN-food-app/screens/MealDetailScreen.js
@@ -25,7 +25,7 @@ function MealDetailScreen({ route, navigation }) {
     const mealIsFavorite = favoriteMealCtx.ids.includes(mealId);
 
     function changeGavoriteStatusHandler() {
-        console.log("pressed!");
+        console.log("pressed!")
         if (mealIsFavorite) {
             favoriteMealCtx.removeFavorite(mealId);
         } else {
@@ -33,7 +33,7 @@ function MealDetailScreen({ route, navigation }) {
         }
     }
 
-    // useLayoutEffect(() => {
+    useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => (
                 <IconButton 
@@ -43,7 +43,7 @@ function MealDetailScreen({ route, navigation }) {
                 />
             )
         });
-    // }, [navigation, changeGavoriteStatusHandler]);
+    }, [navigation, changeGavoriteStatusHandler]);
 
     return (
         <ScrollView style={styles.rootContainer}>
