47f118b92c189237f228b3b93ddc4c91bcdfb752
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-03 17:34:32 +0000

    Dec 3, 2024, 11:04 PM

--- RN-food-app/screens/MealsOverviewScreen.js
+++ RN-food-app/screens/MealsOverviewScreen.js
@@ -22,29 +22,6 @@ function MealsOverviewScreen({ route, navigation }) {
         })
     }, [catId, navigation]);
 
-    function renderMealItem(itemData) {
-        const item = itemData.item;
-        const mealItemProps = {
-            id: item.id,
-            title: item.title,
-            imageURL: item.imageUrl,
-            complexity: item.complexity,
-            duration: item.duration,
-            affordability: item.affordability,
-        }
-        return (
-            <MealItem {...mealItemProps} />
-        );
-    }
-
-    return <View style={styles.container}>
-        <FlatList 
-            data={displayedMeals}
-            keyExtractor={(item) => item.id}
-            renderItem={renderMealItem}
-
-        />
-    </View>
 }
 
 export default MealsOverviewScreen;
