d8aa731594011bfcd55690217053928efbd44f1e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-04 13:11:20 +0000

    Dec 4, 2024, 6:41 PM

--- RN-food-app/screens/FavoritesScreen.js
+++ RN-food-app/screens/FavoritesScreen.js
@@ -25,7 +25,7 @@ function FavoritesScreen() {
         )
     }
     
-    return <MealsList items={favoriteMeals} />
+    return <MealsList items={favoriteMealIds} />
 }
 
 export default FavoritesScreen;
