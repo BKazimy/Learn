60f13b31eaf12bdd5d4a26ca944734685b5375e4
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-04 13:07:26 +0000

    Dec 4, 2024, 6:37 PM

--- RN-food-app/screens/FavoritesScreen.js
+++ RN-food-app/screens/FavoritesScreen.js
@@ -9,10 +9,11 @@ function FavoritesScreen() {
     // const favoriteMealCtx = useContext(FavoritesContext);
     // const favoriteMeals = MEALS.filter(meal => favoriteMealCtx.ids.includes(meal.id));
     
-    const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids)
+    const favoriteMealIds = useSelector((state) => 
+        state.favoriteMeals.ids)
     const favoriteMeals = MEALS.filter((meal) => 
         favoriteMealIds.includes(meal.id)
-);
+    );
     
     if (favoriteMeals.length === 0) {
         return (
