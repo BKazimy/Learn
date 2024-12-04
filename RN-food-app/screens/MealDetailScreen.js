17c0620e87b5421c2d4b0f989c0ec63389d21cc8
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-04 12:53:51 +0000

    Dec 4, 2024, 6:23 PM

--- RN-food-app/screens/MealDetailScreen.js
+++ RN-food-app/screens/MealDetailScreen.js
@@ -19,7 +19,7 @@ import { addFavorite, removeFavorite, favoritesSlice } from "../store/redux/favo
 
 function MealDetailScreen({ route, navigation }) {
     // const favoriteMealCtx = useContext(FavoritesContext);
-    const favoriteMealIds = useSelector((state) => state.fav);
+    const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
     const dispatch = useDispatch();
 
     const mealId = route.params.mealId;
