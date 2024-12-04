34d669d07466e3b2789bb8cdc6708eef381adaa7
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-04 09:38:32 +0000

    Dec 4, 2024, 3:08 PM

--- RN-food-app/App.js
+++ RN-food-app/App.js
@@ -60,7 +60,7 @@ export default function App() {
     <>
       <StatusBar style='light' />
       {/* <FavoritesContextProvider> */}
-      <Provider>
+      <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{
           headerStyle: { backgroundColor: '#351401' },
