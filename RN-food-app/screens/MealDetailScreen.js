53fa20ceb7db8f1f77ee73424258bdf90acfa593
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-04 09:54:24 +0000

    Dec 4, 2024, 3:24 PM

--- RN-food-app/screens/MealDetailScreen.js
+++ RN-food-app/screens/MealDetailScreen.js
@@ -19,6 +19,7 @@ import { useSelector } from "react-redux";
 function MealDetailScreen({ route, navigation }) {
     // const favoriteMealCtx = useContext(FavoritesContext);
     const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+    
 
     const mealId = route.params.mealId;
     const selectedMeal = MEALS.find((meal) => meal.id == mealId);
