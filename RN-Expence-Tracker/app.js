a8dd47d4739d90243d33f6bac7222862b30f5474
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-05 10:08:53 +0000

    Dec 5, 2024, 3:38 PM

--- RN-Expence-Tracker/app.js
+++ RN-Expence-Tracker/app.js
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+
 
 export default function App() {
     return (
