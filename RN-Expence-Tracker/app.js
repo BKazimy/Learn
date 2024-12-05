e26de97f5f25ac0a22ad3f34efc23cff8d8317b4
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-05 10:13:03 +0000

    Dec 5, 2024, 3:43 PM

--- RN-Expence-Tracker/app.js
+++ RN-Expence-Tracker/app.js
@@ -22,8 +22,8 @@ export default function App() {
             <StatusBar style="auto" />
             <NavigationContainer>
                 <Stack.Navigator>
-                    <Stack.Screen name="ManageExpense" component={ManageExpense} />
                     <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+                    <Stack.Screen name="ManageExpense" component={ManageExpense} />
                 </Stack.Navigator>
             </NavigationContainer>
         </>
