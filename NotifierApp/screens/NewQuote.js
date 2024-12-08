35b48ce4b6793efdc36da412370f8dfaa22de29c
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 21:17:31 +0000

    Dec 9, 2024, 2:47 AM

--- NotifierApp/screens/NewQuote.js
+++ NotifierApp/screens/NewQuote.js
@@ -1,9 +1,43 @@
-import { View, Text } from "react-native";
+import React from 'react';
+import {TextInput, StyleSheet} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
-function NewQuote() {
-    return <View>
-        <Text>New Quotes Screen!</Text>
-    </View>
-}
+const MultilineTextInputExample = () => {
+  const [value, onChangeText] = React.useState('Useless Multiline Placeholder');
 
-export default NewQuote;
+  // If you type something in the text box that is a color,
+  // the background will change to that color.
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={[
+          styles.container,
+          {
+            backgroundColor: value,
+          },
+        ]}>
+        <TextInput
+          editable
+          multiline
+          numberOfLines={4}
+          maxLength={40}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          style={styles.textInput}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    padding: 10,
+  },
+});
+
+export default MultilineTextInputExample;
