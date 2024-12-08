7fee43aa8d6ff66329ddaa8c60fe07a616b45fc3
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 21:23:00 +0000

    Dec 9, 2024, 2:53 AM

--- NotifierApp/screens/NewQuote.js
+++ NotifierApp/screens/NewQuote.js
@@ -30,6 +30,7 @@ function NewQuote() {
           onChangeText={text => onChangeText(text)}
           value={value}
           style={styles.textInput}
+          placeholder=''
         />
         <TextInput
           onChangeText={text => onChangeText(text)}
