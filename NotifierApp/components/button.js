3b6af5cb977e32dc3a4ff7f980709e00d9c2ec0b
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 19:46:16 +0000

    Dec 9, 2024, 1:16 AM

--- NotifierApp/components/button.js
+++ NotifierApp/components/button.js
@@ -3,7 +3,7 @@ import { Pressable } from "react-native"
 function CompButton ({ children }) {
     return (
         <Pressable
-            
+            onPress
         >
             {children}
         </Pressable>
