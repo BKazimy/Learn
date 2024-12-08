1c2172a8556367e2ba8a40ba86fac7cd1d3c6a6e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 18:10:40 +0000

    Dec 8, 2024, 11:40 PM

--- NotifierApp/App.js
+++ NotifierApp/App.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 
 function App() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{}}>
       <Stack.Screen 
         name="Home" 
         component={Home} 
