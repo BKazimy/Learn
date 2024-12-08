6032fe9aaf0ea992e81bd70a727a8a15b2868e2f
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 22:04:01 +0000

    Dec 9, 2024, 3:34 AM

--- NotifierApp/screens/NewQuote.js
+++ NotifierApp/screens/NewQuote.js
@@ -4,6 +4,7 @@ import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { View } from 'react-native-web';
 import CompButton from '../components/compButton';
 import Submission from '../components/submission';
+import Form from '../components/form';
 
 function NewQuote({ navigation }) {
     const [quoteValue, setQuoteValue] = React.useState();
@@ -35,44 +36,8 @@ function NewQuote({ navigation }) {
     // If you type something in the text box that is a color,
     // the background will change to that color.
     return (
-      <SafeAreaProvider>
-        <SafeAreaView
-          style={[
-            styles.container,
-            {
-              backgroundColor: quoteValue,
-            },
-          ]}>
-          <TextInput
-            onChangeText={text => setQuoteValue(text)}
-            value={quoteValue}
-            style={styles.textInput}
-            placeholder='Quote:'
-          />
-          <TextInput
-            editable
-            multiline
-            numberOfLines={20}
-            maxLength={500}
-            onChangeText={text => setStoryValue(text)}
-            value={storyValue}
-            style={styles.textInput}
-            placeholder='Example Story:'
-          />
-          <TextInput
-            onChangeText={text => setAuthorValue(text)}
-            value={authorValue}
-            style={styles.textInput}
-            placeholder='Author:'
-          />
-  
-          <Submission
-              onCancel={onCancel}
-              onSave={onSave}
-          />
-  
-        </SafeAreaView>
-      </SafeAreaProvider>
+      <Form 
+      />
     );
 };
 
