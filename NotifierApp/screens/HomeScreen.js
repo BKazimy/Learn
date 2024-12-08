24e275d8a6835fa044b8bdf80bce16b5f21586c3
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 20:37:24 +0000

    Dec 9, 2024, 2:07 AM

--- NotifierApp/screens/HomeScreen.js
+++ NotifierApp/screens/HomeScreen.js
@@ -2,38 +2,40 @@ import { View, Text, Pressable } from "react-native";
 import CompButton from "../components/compButton";
 
 function Home({ navigation }) {
-    return <View>
+    return (
         <View>
-            <CompButton 
-            title="All Quotes"
-            onPress={() => navigation.navigate('AllQuotes')}
-            />
+            <View>
+                <CompButton 
+                title="All Quotes"
+                onPress={() => navigation.navigate('AllQuotes')}
+                />
+            </View>
+            <View>
+                <CompButton 
+                title="Save New Quote"
+                onPress={() => navigation.navigate('NewQuote')}
+                />
+            </View>
+            <View>
+                <CompButton 
+                title="Schedule Notification"
+                onPress={() => navigation.navigate('Schedule')}
+                />
+            </View>
+            <View>
+                <CompButton 
+                title="Random Quote"
+                onPress={() => navigation.navigate('Random')}
+                />
+            </View>
+            <View>
+                <CompButton 
+                title="Today's Quote"
+                onPress={() => navigation.navigate('Todays')}
+                />
+            </View>
         </View>
-        <View>
-            <CompButton 
-            title="Save New Quote"
-            onPress={() => navigation.navigate('NewQuote')}
-            />
-        </View>
-        <View>
-            <CompButton 
-            title="Schedule Notification"
-            onPress={() => navigation.navigate('Schedule')}
-            />
-        </View>
-        <View>
-            <CompButton 
-            title="Random Quote"
-            onPress={() => navigation.navigate('Random')}
-            />
-        </View>
-        <View>
-            <CompButton 
-            title="Today's Quote"
-            onPress={() => navigation.navigate('Todays')}
-            />
-        </View>
-    </View>
+    );
 }
 
 export default Home;
