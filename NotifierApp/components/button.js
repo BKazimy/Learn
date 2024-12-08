dd9e436f8af8ee8bc95314ab36c6d610db262506
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 19:53:22 +0000

    Dec 9, 2024, 1:23 AM

--- NotifierApp/components/button.js
+++ NotifierApp/components/button.js
@@ -2,9 +2,20 @@ import { Pressable } from "react-native"
 
 function CompButton ({ onPress }) {
     return (
-        <Pressable
-            onPress={onPress}
-        />
+        <View style={styles.gridItem}>
+            <Pressable 
+                android_ripple={{color: '#ccc'}}
+                style={({ pressed }) => [
+                    styles.button,
+                    pressed ? styles.buttonPress : null,
+                ]}
+                onPress={onPress}
+            >
+                <View style={[styles.innerContainer, {backgroundColor: color}]}>
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+            </Pressable>
+        </View>
     );
 }
 
