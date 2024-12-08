d9beecad6663148b46e7d85e901ea5ca4177cf80
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 17:56:17 +0000

    Dec 8, 2024, 11:26 PM

--- NotifierApp/App.js
+++ NotifierApp/App.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/HomeScreen';
 import AllQuote from './screens/AllQuotaScreen';
 import NewQuote from './screens/NewQuote';
+import Schedule from './screens/ScheduleScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,7 +12,7 @@ function App() {
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="AllQuotes" component={AllQuote} />
       <Stack.Screen name="NewQuote" component={NewQuote} />
-      <Stack.Screen name="Schedule" component={AllQuote} />
+      <Stack.Screen name="Schedule" component={Schedule} />
       <Stack.Screen name="AllQuotes" component={AllQuote} />
     </Stack.Navigator>
   );
