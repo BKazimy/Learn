e1aacf406f6015b5165f144e7a135e2a4156302b
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 20:28:03 +0000

    Dec 9, 2024, 1:58 AM

--- NotifierApp/vars/color.js
+++ NotifierApp/vars/color.js
@@ -3,7 +3,7 @@ colors = {
     oliveDrab: "#6B8E23",
     darkOliveGreen: "#556B2F",
     darkSeaGreen: "#8FBC8F",
-    seaShell: 'SeaShell',
+    seaShell: '#FFF5EE',
     khaki: "#F0E68C",
     darkKhaki: "#BDB76B",
     dark: "#262425",
