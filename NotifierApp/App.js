5c5d7dbd227a7c756e6c5ed29e24b14563ca48b9
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 18:31:04 +0000

    Dec 9, 2024, 12:01 AM

--- NotifierApp/App.js
+++ NotifierApp/App.js
@@ -5,55 +5,58 @@ import NewQuote from './screens/NewQuote';
 import Schedule from './screens/ScheduleScreen';
 import Random from './screens/RandomScreen';
 import Todays from './screens/TodayQuoteScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen 
-        name="Home" 
-        component={Home} 
-        options={{
-          title: 'Home',
-        }}
-      />
-      <Stack.Screen 
-        name="AllQuotes" 
-        component={AllQuote} 
-        options={{
-          title: 'All Quotes',
-        }}
-      />
-      <Stack.Screen 
-        name="NewQuote" 
-        component={NewQuote} 
-        options={{
-          title: 'Save New Quote',
-        }}
-      />
-      <Stack.Screen 
-        name="Schedule" 
-        component={Schedule} 
-        options={{
-          title: "Schedule Quote Notification",
-        }}
-      />
-      <Stack.Screen 
-        name="Random" 
-        component={Random} 
-        options={{
-          title: "Random Quote",
-        }}
-      />
-      <Stack.Screen 
-        name="Todays" 
-        component={Todays} 
-        options={{
-          title: "Today's Quote",
-        }}
-      />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            title: 'Home',
+          }}
+        />
+        <Stack.Screen 
+          name="AllQuotes" 
+          component={AllQuote} 
+          options={{
+            title: 'All Quotes',
+          }}
+        />
+        <Stack.Screen 
+          name="NewQuote" 
+          component={NewQuote} 
+          options={{
+            title: 'Save New Quote',
+          }}
+        />
+        <Stack.Screen 
+          name="Schedule" 
+          component={Schedule} 
+          options={{
+            title: "Schedule Quote Notification",
+          }}
+        />
+        <Stack.Screen 
+          name="Random" 
+          component={Random} 
+          options={{
+            title: "Random Quote",
+          }}
+        />
+        <Stack.Screen 
+          name="Todays" 
+          component={Todays} 
+          options={{
+            title: "Today's Quote",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
