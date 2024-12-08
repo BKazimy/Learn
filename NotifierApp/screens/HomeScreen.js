2f4b75130c90f94e90127c3dbcb1d28a66efc36b
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 20:53:58 +0000

    Dec 9, 2024, 2:23 AM

--- NotifierApp/screens/HomeScreen.js
+++ NotifierApp/screens/HomeScreen.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
         // flex:1,
         borderColor: 'red',
         borderWidth: 1,
-        // width: "%80",
-        // maxWidth: 720,
+        width: "%80",
+        maxWidth: 720,
     }    
 })
