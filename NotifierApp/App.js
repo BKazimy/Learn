4fc92a31a086f4c0df6b41648105c65b6c139b68
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 22:34:28 +0000

    Dec 9, 2024, 4:04 AM

--- NotifierApp/App.js
+++ NotifierApp/App.js
@@ -13,7 +13,7 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='newquote'>
+      <Stack.Navigator initialRouteName='home'>
         <Stack.Screen 
           name="home" 
           component={Home} 
