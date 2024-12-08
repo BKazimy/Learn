7883b6bf1513d2abce292af4bef80a60e3fd036b
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 21:33:28 +0000

    Dec 9, 2024, 3:03 AM

--- NotifierApp/screens/NewQuote.js
+++ NotifierApp/screens/NewQuote.js
@@ -5,7 +5,7 @@ import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 function NewQuote() {
   const [value, onChangeText] = React.useState();
   const [StoryValue, setStoryValue] = React.useState();
-  const [author, setAuthorValue] = React.useState();
+  const [authorValue, setAuthorValue] = React.useState();
 
   // If you type something in the text box that is a color,
   // the background will change to that color.
