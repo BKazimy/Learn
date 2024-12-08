8d7589a2943e3552a24e861e7e090906e81641d5
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 21:37:28 +0000

    Dec 9, 2024, 3:07 AM

--- NotifierApp/screens/NewQuote.js
+++ NotifierApp/screens/NewQuote.js
@@ -7,7 +7,9 @@ function NewQuote({ edit }) {
   const [StoryValue, setStoryValue] = React.useState();
   const [authorValue, setAuthorValue] = React.useState();
 
-  if (edit)
+  if (edit) {
+    set
+  }
 
   // If you type something in the text box that is a color,
   // the background will change to that color.
