3391b299de142dd67811f33c2a95fde6e08cea9e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 17:46:53 +0000

    Dec 8, 2024, 11:16 PM

---
+++ NotifierApp/App.js
@@ -0,0 +1,14 @@
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default App;
