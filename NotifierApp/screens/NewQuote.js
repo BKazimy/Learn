e3e623091129fecc98df34c0552cce0b6eddb98f
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 21:38:19 +0000

    Dec 9, 2024, 3:08 AM

--- NotifierApp/screens/NewQuote.js
+++ NotifierApp/screens/NewQuote.js
@@ -10,7 +10,7 @@ function NewQuote({ edit }) {
   if (edit) {
     setAuthorValue(edit.author);
     setStoryValue(edit.story);
-    set
+    setQuoteValue(edit.quote)
   }
 
   // If you type something in the text box that is a color,
