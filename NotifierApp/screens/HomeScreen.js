d65af60976568c29c62cef51de619a8a9c67e837
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-08 20:45:22 +0000

    Dec 9, 2024, 2:15 AM

--- NotifierApp/screens/HomeScreen.js
+++ NotifierApp/screens/HomeScreen.js
@@ -5,34 +5,36 @@ function Home({ navigation }) {
     return (
         <View style={styles.outerContainer}>
             <View style={styles.innerContainer}>
-                <CompButton 
-                title="All Quotes"
-                onPress={() => navigation.navigate('AllQuotes')}
-                />
-            </View>
-            <View>
-                <CompButton 
-                title="Save New Quote"
-                onPress={() => navigation.navigate('NewQuote')}
-                />
-            </View>
-            <View>
-                <CompButton 
-                title="Schedule Notification"
-                onPress={() => navigation.navigate('Schedule')}
-                />
-            </View>
-            <View>
-                <CompButton 
-                title="Random Quote"
-                onPress={() => navigation.navigate('Random')}
-                />
-            </View>
-            <View>
-                <CompButton 
-                title="Today's Quote"
-                onPress={() => navigation.navigate('Todays')}
-                />
+                <View>
+                    <CompButton 
+                    title="All Quotes"
+                    onPress={() => navigation.navigate('AllQuotes')}
+                    />
+                </View>
+                <View>
+                    <CompButton 
+                    title="Save New Quote"
+                    onPress={() => navigation.navigate('NewQuote')}
+                    />
+                </View>
+                <View>
+                    <CompButton 
+                    title="Schedule Notification"
+                    onPress={() => navigation.navigate('Schedule')}
+                    />
+                </View>
+                <View>
+                    <CompButton 
+                    title="Random Quote"
+                    onPress={() => navigation.navigate('Random')}
+                    />
+                </View>
+                <View>
+                    <CompButton 
+                    title="Today's Quote"
+                    onPress={() => navigation.navigate('Todays')}
+                    />
+                </View>
             </View>
         </View>
     );
