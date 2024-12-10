241e3d02009f5fbe899ee237b4eb8da53a26ebcb
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 19:56:24 +0000

    Dec 11, 2024, 1:26 AM

--- NotifierApp/App.js
+++ NotifierApp/App.js
@@ -45,7 +45,7 @@ function App() {
             backgroundColor: colors.darkOliveGreen,
           },
           headerTintColor: colors.khaki,
-          headerTintStyle
+          headerTit
         }}
       >
         <Stack.Screen 
