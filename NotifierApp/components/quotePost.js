ff7c1b9e6a04948e0ad0591950ada25a5ee21a19
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 19:11:14 +0000

    Dec 11, 2024, 12:41 AM

--- NotifierApp/components/quotePost.js
+++ NotifierApp/components/quotePost.js
@@ -4,8 +4,8 @@ import colors from "../vars/color";
 function QuotePost({ quote, author, story }) {
     return <View>
         <Text style={[styles.text]}>{quote}</Text>
-        <Text>{author}</Text>
-        <Text>{story}</Text>
+        <Text style={[styles.text]}>{author}</Text>
+        <Text style={[styles.text]}>{story}</Text>
     </View>
 }
 
