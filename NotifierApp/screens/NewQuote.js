d02f37ed9af3be8e39e47f1aab9d41b55bf2d812
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 16:31:35 +0000

    Dec 10, 2024, 10:01 PM

--- NotifierApp/screens/NewQuote.js
+++ NotifierApp/screens/NewQuote.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import {TextInput, StyleSheet} from 'react-native';
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
-import { View } from 'react-native-web';
-import CompButton from '../components/compButton';
-import Submission from '../components/submission';
 import Form from '../components/form';
 
 // import { addQuote } from '../tools/db';
