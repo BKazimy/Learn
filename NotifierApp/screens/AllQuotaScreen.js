7378d59a54ea76c9908e1f183f6652b198e97e0e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 20:04:14 +0000

    Dec 11, 2024, 1:34 AM

--- NotifierApp/screens/AllQuotaScreen.js
+++ NotifierApp/screens/AllQuotaScreen.js
@@ -5,38 +5,7 @@ import Display from "../components/display";
 import CompButton from "../components/compButton";
 
 function AllQuote({ navigation }) {
-    const [data, setdata] = useState([
-        {
-            id: 1,
-            quote: "Never eat the pitza with pinapple!",
-            author: "Arthor Hill",
-            story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        },
-        {
-            id: 2,
-            quote: "blah blah blah",
-            author: "Blah Blah",
-            story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        },
-        {
-            id: 3,
-            quote: "blah blah blah",
-            author: "Blah Blah",
-            story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        },
-        {
-            id: 4,
-            quote: "blah blah blah",
-            author: "Blah Blah",
-            story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        },
-        {
-            id: 5,
-            quote: "blah blah blah",
-            author: "Blah Blah",
-            story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        },
-    ])
+    
     
     function renderCategoryItem(itemData) {
 
