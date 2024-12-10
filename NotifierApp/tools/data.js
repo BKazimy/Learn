ec39ad94894f004335bd2ec0a7fe6ec6593d38b5
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 20:05:48 +0000

    Dec 11, 2024, 1:35 AM

---
+++ NotifierApp/tools/data.js
@@ -0,0 +1,32 @@
+data = [
+    {
+        id: 1,
+        quote: "Never eat the pitza with pinapple!",
+        author: "Arthor Hill",
+        story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+    },
+    {
+        id: 2,
+        quote: "blah blah blah",
+        author: "Blah Blah",
+        story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+    },
+    {
+        id: 3,
+        quote: "blah blah blah",
+        author: "Blah Blah",
+        story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+    },
+    {
+        id: 4,
+        quote: "blah blah blah",
+        author: "Blah Blah",
+        story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+    },
+    {
+        id: 5,
+        quote: "blah blah blah",
+        author: "Blah Blah",
+        story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+    },
+]
