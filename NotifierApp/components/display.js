c3255bdc18994cc81054f925520ef6c841769f41
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 18:56:26 +0000

    Dec 11, 2024, 12:26 AM

--- NotifierApp/components/display.js
+++ NotifierApp/components/display.js
@@ -2,10 +2,7 @@ import { View, StyleSheet } from "react-native";
 
 function Display({ children, customStyle }) {
     return (
-        <View style={[
-                styles.rootContainer,
-                // customStyle
-            ]}>
+        <View style={styles.rootContainer}>
             <View style={styles.outerContainer}>
                 {children}
             </View>
