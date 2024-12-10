91b29d3fbc31ba103aca67f1dad55b2987dffcd6
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 16:44:35 +0000

    Dec 10, 2024, 10:14 PM

--- NotifierApp/screens/AllQuotaScreen.js
+++ NotifierApp/screens/AllQuotaScreen.js
@@ -30,6 +30,20 @@ function AllQuote({ navigation }) {
         },
     ])
     
+    function renderCategoryItem(itemData) {
+        function pressHandler() {
+            navigation.navigate('MealOverview', {
+                categoryId: itemData.item.id,
+            });
+        }
+    
+        return <CategoryGridTile 
+            title={itemData.item.title} 
+            color={itemData.item.color}
+            onPress={pressHandler} 
+        />;
+    }
+    
     return <FlatList 
         data={data} 
         keyExtractor={(item) => item.id} 
