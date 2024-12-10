37e1ffeef55d63ccd247afb156e0996ac3370dc6
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 16:34:09 +0000

    Dec 10, 2024, 10:04 PM

--- NotifierApp/screens/AllQuotaScreen.js
+++ NotifierApp/screens/AllQuotaScreen.js
@@ -2,6 +2,8 @@ import { View, Text } from "react-native";
 import { useState } from "react";
 
 function AllQuote({ navigation }) {
+    const [quoteList, setQuoteList] = useState([])
+
     return <View>
         <Text>All Quotes Screen!</Text>
     </View>
