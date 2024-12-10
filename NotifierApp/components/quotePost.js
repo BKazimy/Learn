9ad6d3071ed02abdff296db117479c7d776beb35
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 19:38:22 +0000

    Dec 11, 2024, 1:08 AM

--- NotifierApp/components/quotePost.js
+++ NotifierApp/components/quotePost.js
@@ -7,7 +7,9 @@ function QuotePost({ quote, author, story }) {
             <Text style={[styles.text, styles.quoteText]}>"{quote}"</Text>
         </View>
         <View style={styles.innerContainer}>
-            <Text style={[styles.text]}>- {author}</Text>
+            <Text style={[styles.text, styles
+                
+            ]}>- {author}</Text>
         </View>
         <View style={[styles.innerContainer, styles.storyContainer]}>
             <Text style={[styles.text, styles.storyText]}>{story}</Text>
