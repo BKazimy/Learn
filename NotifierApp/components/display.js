70231c0f96feda14b4579f85f069c3bf9dd7d831
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 17:04:24 +0000

    Dec 10, 2024, 10:34 PM

---
+++ NotifierApp/components/display.js
@@ -0,0 +1,14 @@
+import { View, StyleSheet } from "react-native";
+import CompButton from "../components/compButton";
+
+function Display({ children }) {
+    return (
+        <View style={styles.rootContainer}>
+            <View style={styles.outerContainer}>
+                
+            </View>
+        </View>
+    );
+}
+
+export default Display;
