8792604c8f663d5a786a0c7194fc0b82e8a6aacc
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 18:53:19 +0000

    Dec 11, 2024, 12:23 AM

--- NotifierApp/components/display.js
+++ NotifierApp/components/display.js
@@ -4,7 +4,7 @@ function Display({ children, customStyle }) {
     return (
         <View style={[
                 styles.rootContainer,
-            
+                
             ]}>
             <View style={styles.outerContainer}>
                 {children}
