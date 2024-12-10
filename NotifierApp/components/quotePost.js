04378ef43308a19edb922ecd364f7859a483e5bd
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 16:54:46 +0000

    Dec 10, 2024, 10:24 PM

--- NotifierApp/components/quotePost.js
+++ NotifierApp/components/quotePost.js
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 
 function QuotePost({ quote, author, story }) {
     return <View>
+        <Text>{id}</Text>
         <Text>{quote}</Text>
         <Text>{author}</Text>
         <Text>{story}</Text>
