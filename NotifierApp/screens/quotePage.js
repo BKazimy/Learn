60f00a5cbf37e4f9dd4300cfd5af6a58f78f1ec9
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-10 17:23:35 +0000

    Dec 10, 2024, 10:53 PM

--- NotifierApp/screens/quotePage.js
+++ NotifierApp/screens/quotePage.js
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 function QuotePage({ id, quote, author, story }) {
     return <View>
         <Text>{id}</Text>
+        <Text>{quote}</Text>
         <Text>{id}</Text>
         <Text>{id}</Text>
     </View>
