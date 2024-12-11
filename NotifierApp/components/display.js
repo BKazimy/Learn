3735c13ca357318728ceb8de0ffd9e659340e36e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 11:25:51 +0000

    Dec 11, 2024, 4:55 PM

--- NotifierApp/components/display.js
+++ NotifierApp/components/display.js
@@ -20,6 +20,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         maxHeight: '90%',
+        overflow: 'hidden',
     },
 
     outerContainer: {
