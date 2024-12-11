e581b2c1de4631c00560d5e6f6c5a5ff776632b2
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 10:54:41 +0000

    Dec 11, 2024, 4:24 PM

--- NotifierApp/components/compButton.js
+++ NotifierApp/components/compButton.js
@@ -15,7 +15,7 @@ function CompButton({ title, onPress, subtitle }) {
             >
                 <View style={[styles.innerContainer, {backgroundColor: colors.darkOliveGreen}]}>
                     <Text style={styles.title}>{title}</Text>
-                    <Text style={styles.authorText}> -{subtitle} {subtitle[1]}</Text>
+                    <Text style={styles.authorText}> -{subtitle}</Text>
                 </View>
             </Pressable>
         </View>
