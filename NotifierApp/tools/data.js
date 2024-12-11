b357f1459c847a1b9ccd431a336a5981b80dec3a
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 18:41:16 +0000

    Dec 12, 2024, 12:11 AM

--- NotifierApp/tools/data.js
+++ NotifierApp/tools/data.js
@@ -16,76 +16,9 @@ class Data {
 
 
 export const Entries = [
-    new Data (
-        1,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
-    new Data (
-        2,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
-    new Data (
-        3,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
-    new Data (
-        4,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
-    new Data (
-        5,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
-    new Data (
-        6,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
-    new Data (
-        7,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
-    new Data (
-        8,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
-    new Data (
-        9,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
-    new Data (
-        10,
-        "Never eat the pitza with pinapple!",
-        "Arthor Hill",
-        'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
-        'abc',
-    ),
+    {
+        
+    }
 ]
 
 export default Entries;
