7e3c0cf7bbc496707ae4a0548ef22fd125dc90ce
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 19:01:58 +0000

    Dec 12, 2024, 12:31 AM

--- NotifierApp/screens/AllQuotaScreen.js
+++ NotifierApp/screens/AllQuotaScreen.js
@@ -1,9 +1,8 @@
 import { FlatList, StyleSheet } from "react-native";
 
 import CompButton from "../components/compButton";
-import QuoteDatabase from "../tools/db";
+// import QuoteDatabase from "../tools/db";
 import { View } from "react-native-web";
-
 
 function AllQuote({ navigation }) {
     
