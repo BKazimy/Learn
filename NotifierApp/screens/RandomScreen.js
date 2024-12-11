8e7258ea09577a4b35320a30c41fc3cb31a5e5cb
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 15:00:33 +0000

    Dec 11, 2024, 8:30 PM

--- NotifierApp/screens/RandomScreen.js
+++ NotifierApp/screens/RandomScreen.js
@@ -1,9 +1,53 @@
-import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+// import { useQuoteContext, QuoteContext } from "../tools/quoteOfDay";
 
-function Random() {
-    return <View>
-        <Text>Random Quotes Screen!</Text>
-    </View>
+import Display from "../components/display";
+import QuotePost from "../components/quotePost";
+import { useLayoutEffect } from "react";
+// import Entries from "../tools/data";
+
+function Todays({ navigation }) {
+    const quoteData = {
+        id: 1,
+        quote: "Never eat the pitza with pinapple!",
+        author: "Arthor Hill",
+        story: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+        source: 'abc',
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: quoteData.author,
+        })
+    })
+
+    return (
+            <Display customStyle={styles.quotePost}>
+                <QuotePost 
+                    id={quoteData.id}
+                    quote={quoteData.quote}
+                    author={quoteData.author}
+                    story={quoteData.story}
+                    source={quoteData.source}
+                />
+            </Display>
+    )
 }
 
-export default Random;
+export default Todays;
+
+const styles = StyleSheet.create({
+    quotePost: {
+        height: 500,
+        borderRadius: 12,
+        backgroundColor: colors.darkOliveGreen,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 16,
+        overflow: 'hidden',
+        padding: 8,
+        
+    }
+})
