edab6e10effa91d613363762b96e8d324a7253b1
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 14:23:29 +0000

    Dec 11, 2024, 7:53 PM

--- NotifierApp/screens/TodayQuoteScreen.js
+++ NotifierApp/screens/TodayQuoteScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { QuoteContext, useQuoteContext } from './contexts/IdContext';
 import Display from "../components/display";
 
