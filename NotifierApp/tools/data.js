d0a54ad8f5ee4e1d455ad2a98d9d0c6beb3f984c
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 18:52:16 +0000

    Dec 12, 2024, 12:22 AM

--- NotifierApp/tools/data.js
+++ NotifierApp/tools/data.js
@@ -1,20 +1,3 @@
-class Data {
-    constructor(
-      id,
-      quote, 
-      author,
-      story,
-      source,
-    ) {
-      this.id = id;
-      this.quote = quote;
-      this.author = author;
-      this.story = story;
-      this.source = source;
-    }
-  }
-
-
 export const Entries = [
         {
           quote: "LAW 1: NEVER OUTSHINE THE MASTER",
