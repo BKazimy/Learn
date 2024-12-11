2dfa204b707c00dd27c214cbdee229f9108ecb35
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 12:19:35 +0000

    Dec 11, 2024, 5:49 PM

--- NotifierApp/screens/AllQuotaScreen.js
+++ NotifierApp/screens/AllQuotaScreen.js
@@ -1,4 +1,4 @@
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 
 import Display from "../components/display";
 import CompButton from "../components/compButton";
