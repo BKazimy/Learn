f124c14200d1c8622dce179dd4e0c5f93f9dc4de
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 19:21:42 +0000

    Dec 12, 2024, 12:51 AM

--- NotifierApp/screens/quotePage.js
+++ NotifierApp/screens/quotePage.js
@@ -8,7 +8,7 @@ import colors from "../vars/color";
 function QuotePage({ route, navigation, id }) {
 
     const { db } = route.params
-    const quoteData = route.params;
+    const quoteData = db.
 
     useLayoutEffect(() => {
         navigation.setOptions({
