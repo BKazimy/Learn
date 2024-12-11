8bbdf7c1aa6e319e3561eb66f83dee79e5cd673e
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 18:41:36 +0000

    Dec 12, 2024, 12:11 AM

--- NotifierApp/tools/data.js
+++ NotifierApp/tools/data.js
@@ -16,9 +16,36 @@ class Data {
 
 
 export const Entries = [
-    {
-        
-    }
+        {
+          quote: "LAW 1: NEVER OUTSHINE THE MASTER",
+          author: "Robert Greene",
+          story: "Always make those above you feel comfortably superior. In your desire to please and impress them, do not go too far in displaying your talents or you might accomplish the opposite—inspire fear and insecurity. Make your masters appear more brilliant than they are and you will attain the heights of power.",
+          source: "The 48 Laws of Power",
+        },
+        {
+          quote: "LAW 2: NEVER PUT TOO MUCH TRUST IN FRIENDS, LEARN HOW TO USE ENEMIES",
+          author: "Robert Greene",
+          story: "Be wary of friends—they will betray you more quickly, for they are easily aroused to envy. They also become spoiled and tyrannical. But hire a former enemy and he will be more loyal than a friend, because he has more to prove. In fact, you have more to fear from friends than from enemies. If you have no enemies, find a way to make them.",
+          source: "The 48 Laws of Power",
+        },
+        {
+          quote: "LAW 3: CONCEAL YOUR INTENTIONS",
+          author: "Robert Greene",
+          story: "Keep people off-balance and in the dark by never revealing the purpose behind your actions. If they have no clue what you are up to, they cannot prepare a defence. Guide them far enough down the wrong path, envelop them in enough smoke, and by the time they realize your intentions, it will be too late.",
+          source: "The 48 Laws of Power",
+        },
+        {
+          quote: "LAW 4: ALWAYS SAY LESS THAN NECESSARY",
+          author: "Robert Greene",
+          story: "When you are trying to impress people with words, the more you say, the more common you appear, and the less in control. Even if you are saying something banal, it will seem original if you make it vague, open-ended, and sphinxlike. Powerful people impress and intimidate by saying less. The more you say, the more likely you are to say something foolish.",
+          source: "The 48 Laws of Power",
+        },
+        {
+          quote: "LAW 5: SO MUCH DEPENDS ON REPUTATION—GUARD IT WITH YOUR LIFE",
+          author: "Robert Greene",
+          story: "Reputation is the cornerstone of power. Through reputation alone you can intimidate and win; once it slips, however, you are vulnerable, and will be attacked on all sides. Make your reputation unassailable. Always be alert to potential attacks and thwart them before they happen. Meanwhile, learn to destroy your enemies by opening holes in their own reputations. Then stand aside and let public opinion hang them.",
+          source: "The 48 Laws of Power",
+        },
 ]
 
 export default Entries;
