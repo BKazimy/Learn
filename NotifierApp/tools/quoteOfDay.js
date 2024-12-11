a0e346fe130f76900ea049909ee912ae1d403a63
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 14:19:43 +0000

    Dec 11, 2024, 7:49 PM

--- NotifierApp/tools/quoteOfDay.js
+++ NotifierApp/tools/quoteOfDay.js
@@ -4,9 +4,9 @@ import React, { createContext, useState, useContext } from 'react';
 const IdContext = createContext();
 
 // Create a provider component that will wrap the app or part of it
-export const IdProvider = ({ children }) => {
+export const QuoteOfDay = ({ children }) => {
   // State to hold the id
-  const [id, setId] = useState(null);
+  const [id, setId] = useState(1);
 
   // Function to update the id
   const updateId = (newId) => {
