110fd3bca18affe36f9bf1646fbc83cf1decd292
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 14:23:19 +0000

    Dec 11, 2024, 7:53 PM

--- NotifierApp/screens/TodayQuoteScreen.js
+++ NotifierApp/screens/TodayQuoteScreen.js
@@ -19,3 +19,19 @@ function Todays() {
 }
 
 export default Todays;
+
+const styles = StyleSheet.create({
+    quotePost: {
+        height: 500,
+        borderRadius: 12,
+        backgroundColor: colors.darkOliveGreen,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 16,
+        overflow: 'hidden',
+        padding: 8,
+        
+    }
+})
