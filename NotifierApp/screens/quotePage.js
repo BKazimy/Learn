c1636398ecf5b33a148c572340f46299225baa20
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 20:31:32 +0000

    Dec 12, 2024, 2:01 AM

--- NotifierApp/screens/quotePage.js
+++ NotifierApp/screens/quotePage.js
@@ -9,8 +9,7 @@ function QuotePage({ route, navigation }) {
     const { db, id } = route.params;
     const [quoteData, setQuoteData] = useState(null); // State to store the fetched data
 
-    if (id) {}
-    console.log('id:', id);
+    if (id) {console.log('id:', id);}
     // Fetch data when the component mounts
     useEffect(() => {
         const fetchData = async () => {
