7bc5082b14c575a1ed19259a582b63705153274c
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 11:14:42 +0000

    Dec 11, 2024, 4:44 PM

--- NotifierApp/components/compButton.js
+++ NotifierApp/components/compButton.js
@@ -57,6 +57,7 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: colors.darkOliveGreen,
     },
 
     title: {
