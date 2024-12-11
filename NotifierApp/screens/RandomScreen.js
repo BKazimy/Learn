e22672074ef2aee229c3941e7dd85ad774b42a71
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-11 20:05:43 +0000

    Dec 12, 2024, 1:35 AM

--- NotifierApp/screens/RandomScreen.js
+++ NotifierApp/screens/RandomScreen.js
@@ -1,8 +1,6 @@
 import { StyleSheet } from "react-native";
 // import { useQuoteContext, QuoteContext } from "../tools/quoteOfDay";
 
-import Display from "../components/display";
-import QuotePost from "../components/quotePost";
 
 function RandomQuote({ navigation }) {
 
