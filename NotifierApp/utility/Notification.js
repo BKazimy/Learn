93bcdd68011e8515dfd5957788ae69d4293f1ff4
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-12 22:26:01 +0000

    Dec 13, 2024, 3:56 AM

--- NotifierApp/utility/Notification.js
+++ NotifierApp/utility/Notification.js
@@ -50,7 +50,7 @@ export const ScheduleNotification = async (db) => {
     await Notifications.cancelAllScheduledNotificationsAsync();
 
     // Access the current quote ID from the database
-    const { id } = db.GetQuoteOfDay();
+    const { id } = db.Setquo();
     if (!id) {
       console.error('No ID found in context.');
       return;
