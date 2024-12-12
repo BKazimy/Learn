3f9f1d52ea0676346c40ccefcad12904cd517501
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-12 23:52:03 +0000

    Dec 13, 2024, 5:22 AM

--- NotifierApp/utility/db.js
+++ NotifierApp/utility/db.js
@@ -143,7 +143,7 @@ class QuoteDatabase {
       idObj = {id: data.id};
       // console.log('setquote id:', id);
       await AsyncStorage.setItem('today', JSON.stringify(idObj));
-      return ;
+      return idobj.id;
     } catch (e) {
       console.log('Couldnt update quote of day!', e);
       return this.SetQuoteOfDay();
