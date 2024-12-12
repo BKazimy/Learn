cf0cd54322493928e6fa61e86c56d7ba0ecaafa7
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-12 23:58:02 +0000

    Dec 13, 2024, 5:28 AM

--- NotifierApp/App.js
+++ NotifierApp/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect } from 'react';
+import React, { useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
