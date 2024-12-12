d0115de349523ea616000d2cd6bbaea5b1ab966b
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-12 22:20:13 +0000

    Dec 13, 2024, 3:50 AM

--- NotifierApp/screens/quotePage.js
+++ NotifierApp/screens/quotePage.js
@@ -18,6 +18,7 @@ function QuotePage({ route, navigation }) {
                     data = await db.getById(id); // Fetch quote data
                 } else {
                     data = await db.getRandom(); // Fetch quote data
+                    // send the notification for 
                     await scheduleNotification(data.quote, data.story, data.id);
                 }
                 setQuoteData(data); // Update state with fetched data
