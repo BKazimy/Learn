ecdf830a8f85b96196bc850da9ceb75eb69f8ece
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-13 13:31:32 +0000

    Dec 13, 2024, 7:01 PM

--- NotifierApp/screens/quotePage.js
+++ NotifierApp/screens/quotePage.js
@@ -35,7 +35,7 @@ function QuotePage({ route, navigation }) {
     
             console.log('Came from newQuote, navigating to home.');
             // navigation.navigate('home'); // Navigate to home
-            navigateToHome();
+            resetAction();
             console.log('went home!');
           } else {
             console.log('Default goBack behavior triggered.');
