ea292ca675b75dc33c6f4b35b8f14131c2f45b65
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-13 17:08:28 +0000

    Dec 13, 2024, 10:38 PM

--- NotifierApp/screens/HomeScreen.js
+++ NotifierApp/screens/HomeScreen.js
@@ -56,7 +56,7 @@ function Home({ navigation, route }) {
             </View>
             <View>
                 <CompButton 
-                    title="Report an Issue"
+                    title="Contact"
                     onPress={() => Linking.openURL('https://www.instagram.com/b_kazimy_')}
                 />
             </View>
