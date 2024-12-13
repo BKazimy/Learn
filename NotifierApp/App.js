c7437ed8d27a01bc1c672a033d01b76274fca4b1
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-13 00:25:02 +0000

    Dec 13, 2024, 5:55 AM

--- NotifierApp/App.js
+++ NotifierApp/App.js
@@ -23,7 +23,7 @@ function App() {
   const fetchId = async () => {
     let a = await db.GetQuoteOfDay();
     console.log('appjs id:', a);
-    setId(a);
+    setId(a.id);
   }
 
   useEffect(() => {
