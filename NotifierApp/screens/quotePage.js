b86c6a1a919666ee6c2b044a8ac100894ac9fd39
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-13 13:01:59 +0000

    Dec 13, 2024, 6:31 PM

--- NotifierApp/screens/quotePage.js
+++ NotifierApp/screens/quotePage.js
@@ -29,7 +29,7 @@ function QuotePage({ route, navigation }) {
     
             console.log('Came from newQuote, navigating to home.');
             // navigation.navigate('home'); // Navigate to home
-            navigation.dispatch(CommonActions.reset({index: 0, routes: [{ name: 'home' }]}));
+            navigation.dispatch();
           } else {
             console.log('Default goBack behavior triggered.');
             navigation.dispatch(e.data.action); // Allow the default back action
