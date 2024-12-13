da7111d9ae7c70d857c7495e308bd9976dd485d5
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-13 00:32:57 +0000

    Dec 13, 2024, 6:02 AM

--- NotifierApp/screens/quotePage.js
+++ NotifierApp/screens/quotePage.js
@@ -9,7 +9,7 @@ import colors from "../utility/color";
 import { SendNotification } from "../utility/Notification";
 
 function QuotePage({ route, navigation }) {
-    const { db, id } = route.params;
+    const db = route.params.db;
     const [quoteData, setQuoteData] = useState(null); // State to store the fetched data
 
     // Fetch data when the component mounts
