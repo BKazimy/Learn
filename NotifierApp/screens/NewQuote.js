9de1b604fe011f5a4cbe9138fd23f2672eff6aea
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-13 14:04:53 +0000

    Dec 13, 2024, 7:34 PM

--- NotifierApp/screens/NewQuote.js
+++ NotifierApp/screens/NewQuote.js
@@ -1,9 +1,32 @@
+import React, { useCallback } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+
 // components
 import Form from '../components/form';
 import Display from '../components/display';
 
 function NewQuote({ navigation, route }) {
   const { db } = route.params;
+
+  
+  useFocusEffect(
+    useCallback(() => {
+      const state = navigation.getState();
+      console.log('Navigation State:', state);
+  
+      // Ensure we safely access the previous route
+      const previousRoute = state.routes[state.index - 1] || null;
+
+      if (previousRoute) {
+        console.log('Came from Route:', previousRoute.name);
+        if (previousRoute.name === 'quotePage') {
+          navigation.navigate('home');
+        }
+      } else {
+        console.log('No previous route. This might be the initial screen.');
+      }
+    }, [navigation])
+  );
 
   async function newQuoteHandler(quote, author, story = '', source) {
     try {
