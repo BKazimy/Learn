6a16035975d857319ab2bb45cff0911d1c8eff75
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-13 01:13:11 +0000

    Dec 13, 2024, 6:43 AM

--- NotifierApp/screens/HomeScreen.js
+++ NotifierApp/screens/HomeScreen.js
@@ -15,7 +15,7 @@ function Home({ navigation, route }) {
         // const { today } = route.params;
         // const id = today;
         // console.log('home today:', a);
-        console.log('home today:', db.t);
+        console.log('home today:', db.GetQuoteOfDay());
     }
     fetchId(); 
 
