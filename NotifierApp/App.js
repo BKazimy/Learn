65e9c36f1dad20010651fa58177157956cfa9419
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-13 00:39:41 +0000

    Dec 13, 2024, 6:09 AM

--- NotifierApp/App.js
+++ NotifierApp/App.js
@@ -18,7 +18,7 @@ const Stack = createStackNavigator();
 
 function App() {
   let navigation = useNavigation;
-  const [id, setId] = useState();
+  const [todays, setId] = useState();
 
   const fetchId = async () => {
     let a = await db.GetQuoteOfDay();
