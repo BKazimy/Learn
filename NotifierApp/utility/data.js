68b0721b8a12c50890a718a43562303fe427862a
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-15 16:50:52 +0000

    Dec 15, 2024, 10:20 PM

--- NotifierApp/utility/data.js
+++ NotifierApp/utility/data.js
@@ -71,6 +71,12 @@ const Entries = [
     story: "",
     source: "Art Of War",
   },
+  {
+    quote: "1. Laying plans: ",
+    author: "Sun Tzu",
+    story: "",
+    source: "Art Of War",
+  },
         {
           quote: "LAW 1: NEVER OUTSHINE THE MASTER",
           author: "Robert Greene",
