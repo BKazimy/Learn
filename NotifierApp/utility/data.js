e6239317e60ae7d8f9adc584e4fac3d5f37de3f2
Besmella Kazimy <b.kazimy5@gmail.com>
2024-12-15 16:26:25 +0000

    Dec 15, 2024, 9:56 PM

--- NotifierApp/utility/data.js
+++ NotifierApp/utility/data.js
@@ -1,4 +1,10 @@
 const Entries = [
+  {
+    quote: "LAW 1: NEVER OUTSHINE THE MASTER",
+    author: "Robert Greene",
+    story: "Always make those above you feel comfortably superior. In your desire to please and impress them, do not go too far in displaying your talents or you might accomplish the opposite—inspire fear and insecurity. Make your masters appear more brilliant than they are and you will attain the heights of power.",
+    source: "The 48 Laws of Power",
+  },
         {
           quote: "LAW 1: NEVER OUTSHINE THE MASTER",
           author: "Robert Greene",
